a0a82adf985db1e766cc50a40106583601b4d60f
dhinadhina-23 <dhinasubramaniyam.3@gmail.com>
2025-12-16 11:34:59 +0530

    solve the problem of result.html back to upload button

--- santa_app/forms.py
+++ santa_app/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class EmployeeUploadForm(forms.Form):
     csv_file = forms.FileField(label='Upload Employee CSV')
+
+    def clean_csv_file(self):
+        file = self.cleaned_data['csv_file']
+        if not file.name.endswith('.csv'):
+            raise forms.ValidationError('Only CSV files are allowed')
+        return file
